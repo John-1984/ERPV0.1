92b07ce7a61cc47af961d98ff539cea5555feb3c
John <jojojos@gmail.com>
2019-02-15 16:12:17 +0530

    Stored Procedure call using Entity Framework implemented. Login flow wiredup with EF and SP.

---
+++ DataLayer/UserDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace DataLayer
+{
+    public class UserDAL
+    {
+        public UserDAL()
+        {
+        }
+
+        public BusinessModels.User GetUser(string userName, string password) {
+            var _user = new BusinessModels.User();
+            using (var dbContext = new UserDbContext())
+            {
+                _user = dbContext.User
+                                .FirstOrDefault(p => p.UserName.Equals(userName) && p.Password.Equals(password));
+                             
+            }
+            return _user;
+        }
+
+        public BusinessModels.User ValidateUser(string userName, string password)
+        {
+            var _user = new BusinessModels.User();
+            try             {                 using (var dbContext = new UserDbContext())                 {
+                    _user = dbContext.Database.SqlQuery<BusinessModels.User>("CALL VerifyUsernameAndPassword(@_userName, @_password)", new MySqlParameter("@_userName", userName), new MySqlParameter("@_password", password)).FirstOrDefault();                 }             }             catch (Exception ex)             {                 var test = ex.Message;             }
+            return _user;
+        }
+    }
+}
---
+++ DataLayer/UserDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DataLayer
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public partial class UserDbContext: DbContext
+    {
+        public UserDbContext() : base("LocalMySqlServer")
+        {
+        }
+
+        public DbSet<BusinessModels.User> User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
